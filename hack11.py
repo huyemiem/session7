adfaacf5b6661d8fd591b28f689b5c2a876af24a
huyemiem <54630019+huyemiem@users.noreply.github.com>
2019-09-16 18:58:12 +0700

    Add files via upload

---
+++ hack11.py
@@ -0,0 +1,11 @@
+a=input("Nhap ten:")
+b=input("Nhap mat khau:")
+c=input("Nhap email:")
+d=input("Nhap lai mat khau:")
+
+while True:
+    if b==d:
+        print("Nhap mat khau thanh cong")
+        break
+    else:
+        print("Yeu cau nhap lai mat khau")    
